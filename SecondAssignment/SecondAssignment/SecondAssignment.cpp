7859d6c3b904052165783db69fd4bd85794fc540
Okan Koca <137695882+OkanKoca@users.noreply.github.com>
2024-01-21 20:40:57 +0300

    Update SecondAssignment.cpp

--- SecondAssignment/SecondAssignment/SecondAssignment.cpp
+++ SecondAssignment/SecondAssignment/SecondAssignment.cpp
@@ -1,14 +1,4 @@
-/****************************************************************************
-**					           SAKARYA ÜNÝVERSÝTESÝ
-**			         BÝLGÝSAYAR VE BÝLÝÞÝM BÝLÝMLERÝ FAKÜLTESÝ
-**				          BÝLGÝSAYAR MÜHENDÝSLÝÐÝ BÖLÜMÜ
-**				            PROGRAMLAMAYA GÝRÝÞÝ DERSÝ
-**
-**				     ÖDEV NUMARASI…...: 2
-**				     ÖÐRENCÝ ADI...............: Okan Koca
-**				     ÖÐRENCÝ NUMARASI.: B231210016
-**				     DERS GRUBU…………: C
-****************************************************************************/
+
 
 #include <iostream>
 #include <vector>
